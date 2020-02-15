b204fc1c820b0012f3c450e38864f38cefa2f5fc
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2020-02-15 20:00:28 -0300

    Organização do código.
    Tudo organizado, falta comentar melhor e adicionar mais funcionalidades.

--- main.py
+++ main.py
@@ -1,107 +1,115 @@
-# Tente executar o codigo da variavel x.
-try:
+# Exemplo de código que já funciona
+def codigo():
+    cod = '''
+    
+sla = 133
 
-    x = '''
+if sla == 13:
+    print sla
+   
+    
+    print len(str(sla))
+    
+    '''
+    return cod
 
-# somadora4.py - somadora infinita - versao 4 
-print 'Digite os valores a somar seguidos de [ENTER].'
-print 'Para encerrar apenas [ENTER].'
-total = 0
-while 1:
-    try:
-        linha = input(':')
-        n = float(linha)
-        total = total + n
-    except:
-        if len(linha) == 0:
-            break
-        elif ',' in linha:
-            print 'Use o . (ponto) como separador decimal.'
+
+class Corrigir:
+
+    def __init__(self, *args, converter=True):
+        cod = list(args)
+
+        # print(f'Código(s) corrigido(s) >> {cod}')
+
+        try:
+            # Abre o arquivo de texto ou o que for passado como texto.
+            x = codigo()
+
+            # Executa o texto.
+            exec(x)
+
+        except SyntaxError as erro:
+            # Retorna a mensage de erro em str.
+            mensage = erro.msg
+            # retorna o print()...S2
+            pri = erro.msg[53:64]
+            # Me retorna o código errado em str
+            texto = erro.text
+            # Retorna a linha do erro em int
+            linha = erro.lineno
+            # Retorna quantas letras tem o código errado em int
+            letras = erro.offset
+            # retorna o que tipo é, tem que estudar + n entedi direito
+            sla = erro.filename
+
+            # Me retorna o print corrigido, dentro da variável test em forma de str.
+            aaa = len(mensage)
+            test = mensage[53:aaa - 1]  # Fatia a area do print('') junto com o texto entre ''
+
+            # O erro tem 63 letras contando de 0.
+            print(f"Parênteses ausentes na chamada para 'print'. Tratando para você.\n ... Tratando erros")
+            print('-----------Apartir daqui o código corrigido------------------------')
+            # Cria uma lista
+            y = x.split('\n')
+            print()
+
+            #    print(texto)
+
+            #    x = x.replace(texto, test)
+            # Bora achar os prints
+            ad = 0
+            for g in y:
+                ad += 1
+                # A variável acha vai me retorna a posição inicial do print.
+                acha = g.find('print')
+                if acha >= 1:
+                    ad += 1
+
+                    print(f'Erro tratado >>> {y[ad - 2][acha:]} na linha {ad - 2}.')
+                    # Coloca na lista com o print formatado e corrigido.
+                    y[ad - 2] = f'{y[ad - 2][0:acha]}print({y[ad - 2][5 + acha + 1:]})'
+                    ad -= 1
+                    #
+
+                # print(f'{g[0:5]}')
+
+                # Acessa o que esta dentro do print.
+                # g vai me retornar o erro.
+
+                if g[0:5] == 'print':
+                    ad += 1
+
+                    # Tem que ser menos 1, porque a lista começa de 0
+                    print(f'Erro tratado {g} >>> print({y[ad - 2][6:]}) na linha {ad - 2}.')
+
+                    # Coloca o print corrigido na tabela.
+                    y[ad - 2] = f'print({y[ad - 2][6:]})'
+                    #
+                    # print(f'{len(y[ad-1])}', end='\n')
+                    ad -= 1
+                    # y é a nossa tabela corrigida.
+
+            self.cov(conv=converter, yy=y)
+
+    def cov(self, conv, yy):
+        # Se a pessoa não definine ele executa o código por padrão
+        # yy é nossa tabela corrigida.
+        # Junta a lista em uma string.
+        if conv:
+            corrigido = '\n'.join(map(str, yy))
+            print('----------------------------\nCódigo corrigido')
+            print(f'\n\n {corrigido}')
+
+            print('------Executando código corrigido-----------')
+            exec(corrigido)
         else:
-             print 'Isso nao parece um numero valido.'
-print 'TOTAL: %s' % total
-
-'''
-    # Executa o texto.
-    exec(x)
-
-except SyntaxError as erro:
-    # Retorna a mensagem de erro em str.
-    mensage = erro.msg
-    # retorna o print()...S2
-    pri = erro.msg[53:64]
-    # Me retorna o codigo errado em str
-    texto = erro.text
-    # Retorna a linha do erro em int
-    linha = erro.lineno
-    # Retorna quantas letras tem o codigo errado em int
-    letras = erro.offset
-    # retorna o que tipo é, tem que estudar + n entedi direito
-    sla = erro.filename
-
-    # Me retorna o print corrrigido, dentro da variavel test em forma de str.
-    aaa = len(mensage)
-    test = mensage[53:aaa-1] # Fatia a area do print('') junto com o texto entre ''
-
-    # O erro tem 63 letras contando de 0.
-    print(f"Parênteses ausentes na chamada para 'print'. Tratando para você.\n ... Tratando erros")
-    print('-----------Apartir daqui o codigo corrigido------------------------')
-    # Cria uma lista
-    y = x.split('\n')
-    print()
-
-#    print(texto)
-
-#    x = x.replace(texto, test)
-    # Bora achar os prints
-    ad = 0
-    for g in y:
-        ad += 1
-        # A variavel acha vai me retorna a posição inicial do print.
-        acha = g.find('print')
-        if acha >= 1:
-            ad += 1
-
-            print(f'Erro tratado >>> {y[ad-2][acha:]} na linha {ad-2}.')
-            # Coloca na lista com o print formatado e corrigido.
-            y[ad - 2] = f'{y[ad - 2][0:acha]}print({y[ad-2][5+acha+1:]})'
-            ad -= 1
-
-            #
-        #print(f'{g[0:5]}')
-
-        # Acessa o que esta dentro do print.
-        # g vai me retornar o erro.
-
-        if g[0:5] == 'print':
-            ad += 1
-
-            # Tem que ser menos 1, porque a lista começa de 0
-            print(f'Erro tratado {g} >>> print({y[ad-2][6:]}) na linha {ad-2}.')
-
-            # Coloca o print corrigido na tabela.
-            y[ad-2] = f'print({y[ad-2][6:]})'
-            #
-            # print(f'{len(y[ad-1])}', end='\n')
-            ad -= 1
+            corrigido = '\n'.join(map(str, yy))
+            print('----------------------------\nCódigo corrigido')
+            print(f'\n\n {corrigido}')
+            print('----------Tabelamento------?')
+            print(yy)
 
 
-    #######1234
+if __name__ == '__main__':
 
-
-#    print(f'\n\n {corrigido}')
-
-
-
-# Deixa para executar o codigo depois
-    # Junta a lista em uma string.
-    rrr = 't'
-    if rrr == 't':
-        corrigido = '\n'.join(map(str, y))
-        print('----------------------------\nCodigo corrigido')
-        print(f'\n\n {corrigido}')
-
-        print('------Executando codigo corrigido-----------')
-        exec(corrigido)
-    else:
-        print(y)
+    Corrigir()
